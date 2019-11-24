d53c531d4faeb0c962b3f32d93d87a089b61b9c7
Sufi Ahmed Hamim <sufiahmed.hamim@gmail.com>
2019-11-24 14:13:34 +0600

    migration file changed

--- database/migrations/2019_10_26_174418_alter_user_book_table.php
+++ database/migrations/2019_10_26_174418_alter_user_book_table.php
@@ -13,37 +13,37 @@ class AlterUserBookTable extends Migration
      */
     public function up()
     {
-        Schema::create('bookmarked', function (Blueprint $table) {
+        // Schema::create('bookmarked', function (Blueprint $table) {
             
-            $table->foreign('user_id')
-                ->references('id')->on('users')
-                ->onDelete('cascade');
-            $table->foreign('book_id')
-                ->references('id')->on('books')
-                ->onDelete('cascade');
-        });
+        //     $table->foreign('user_id')
+        //         ->references('id')->on('users')
+        //         ->onDelete('cascade');
+        //     $table->foreign('book_id')
+        //         ->references('id')->on('books')
+        //         ->onDelete('cascade');
+        // });
 
-        Schema::create('review_books', function (Blueprint $table) {
+        // Schema::create('review_books', function (Blueprint $table) {
             
-            $table->foreign('user_id')
-                ->references('id')->on('users')
-                ->onDelete('cascade');
-            $table->foreign('book_id')
-                ->references('id')->on('books')
-                ->onDelete('cascade');
-        });
+        //     $table->foreign('user_id')
+        //         ->references('id')->on('users')
+        //         ->onDelete('cascade');
+        //     $table->foreign('book_id')
+        //         ->references('id')->on('books')
+        //         ->onDelete('cascade');
+        // });
 
-        Schema::create('books', function (Blueprint $table) {
-            $table->foreign('cat_id')
-                ->references('id')->on('categories')
-                ->onDelete('cascade');
-        });
+        // Schema::create('books', function (Blueprint $table) {
+        //     $table->foreign('cat_id')
+        //         ->references('id')->on('categories')
+        //         ->onDelete('cascade');
+        // });
 
-        Schema::create('categories', function (Blueprint $table) {
-            $table->foreign('parent_id')
-                ->references('id')->on('categories')
-                ->onDelete('cascade');
-        });
+        // Schema::create('categories', function (Blueprint $table) {
+        //     $table->foreign('parent_id')
+        //         ->references('id')->on('categories')
+        //         ->onDelete('cascade');
+        // });
     }
 
     /**
